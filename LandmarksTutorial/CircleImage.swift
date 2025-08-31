c76b657d0aee42431da853227ea605ddc1eff533
Seoincam <seoin4715@naver.com>
2025-08-31 16:53:50 +0900

    01. Creating and combining views

---
+++ LandmarksTutorial/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  LandmarksTutorial
+//
+//  Created by 박서인 on 8/31/25.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
