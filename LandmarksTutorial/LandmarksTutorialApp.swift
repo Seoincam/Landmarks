d6e8208d2631ec10a2f00609b6cd7a42f827a1bf
Seoincam <seoin4715@naver.com>
2025-08-31 14:53:47 +0900

    Initial Commit

---
+++ LandmarksTutorial/LandmarksTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksTutorialApp.swift
+//  LandmarksTutorial
+//
+//  Created by 박서인 on 8/31/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
