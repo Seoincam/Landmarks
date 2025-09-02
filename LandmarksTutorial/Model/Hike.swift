ea2b68e534d10cee32b25e5fd695e45f6a155c07
Seoincam <seoin4715@naver.com>
2025-09-02 10:03:50 +0900

    05. Animating views and transitions

---
+++ LandmarksTutorial/Model/Hike.swift
@@ -0,0 +1,31 @@
+//
+//  Hike.swift
+//  LandmarksTutorial
+//
+//  Created by 박서인 on 9/2/25.
+//
+
+import Foundation
+
+struct Hike: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    var distance: Double
+    var difficulty: Int
+    var observations: [Observation]
+    
+    static var formatter = LengthFormatter()
+    
+    var distanceText: String {
+        Hike.formatter
+            .string(fromValue: distance, unit: .kilometer)
+    }
+    
+    struct Observation: Hashable, Codable {
+        var distanceFromStart: Double
+        
+        var elevation: Range<Double>
+        var pace: Range<Double>
+        var heartRate: Range<Double>
+    }
+}
